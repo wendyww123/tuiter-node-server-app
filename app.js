8fc30739befcf81629b2aaa18f348b0ff701b29d
yingwen Wen <yingwenwen@MacBook-Air-9.local>
2023-04-09 21:28:47 -0700

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import cors from 'cors'
-import HelloController from './controllers/hello-controller.js';
-import UserController from "./controllers/users/users-controller.js";
-import TuitsController from './controllers/tuits/tuits-controller.js';
-
 
 const app = express();
-app.use(cors())
-app.use(express.json());
-TuitsController(app);
-HelloController(app);
-UserController(app);
-app.listen(process.env.PORT || 4000);
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+app.get('/hello', (req, res) => {
+    res.send('Life is good!')
+})
+app.get('/', (req, res) => {
+    res.send('Welcome to Full Stack Development!')
+})
+app.listen(4000);
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,23 +1,28 @@
-export default [
-    {
-        "_id": "123", "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.jpeg",
-        "title": "SpaceX's Mission",
-        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345, "replies": 123, "retuits": 432
-    },
-    {
-        "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "image": "BoringCo.png",
-        "title": "The Boring Company fixing traffic",
-        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-        "liked": true, "likes": 2345, "replies": 321, "retuits": 432
-    },
-    {
-        "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-        "handle": "@neurolink", "time": "3h", "image": "tesla.png",
-        "title": "Neurolink fixes paralisis and blindness",
-        "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
-        "liked": true, "likes": 3456, "replies": 345, "retuits": 654
-    },
-];
+import * as tuitsDao from '../../tuits/tuits-dao.js'
+
+const findTuits = async (req, res) => {
+    const tuits = await tuitsDao.findTuits()
+    res.json(tuits);
+}
+const createTuit = async (req, res) => {
+    const newTuit = req.body;
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    const insertedTuit = await tuitsDao
+        .createTuit(newTuit);
+    res.json(insertedTuit);
+}
+const deleteTuit = async (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    const status = await tuitsDao
+        .deleteTuit(tuitdIdToDelete);
+    res.json(status);
+    const updateTuit = async (req, res) => {
+        const tuitdIdToUpdate = req.params.tid;
+        const updates = req.body;
+        const status = await tuitsDao
+            .updateTuit(tuitdIdToUpdate,
+                updates);
+        res.json(status);
+    }
+}
